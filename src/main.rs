c35097dd1233700640bd6519cf6cbf7cb6dea86c
Vladimir Kudiurkin <zloyholodec@gmail.com>
2023-04-04 19:33:34 +0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use core::fmt;
-use std::{time::Instant, cmp::min};
+use std::{cmp::min, time::Instant};
 
-type BoardArray = [u8; 16];
+const BOARD_SIZE: usize = 4;
+
+type BoardArray = [u8; BOARD_SIZE * BOARD_SIZE];
 
 #[derive(Clone)]
 struct BoardState {
@@ -12,9 +14,8 @@ struct BoardState {
     down_done: bool,
     zero_pos: usize,
     range: u32,
-    hash: usize
+    hash: usize,
 }
-
 
 impl BoardState {
     fn is_diff_board(&self, other: &Self) -> bool {
@@ -24,7 +25,7 @@ impl BoardState {
 
         for i in 0..self.board.len() {
             if self.board[i] != other.board[i] {
-                return  true;
+                return true;
             }
         }
 
@@ -45,9 +46,13 @@ impl BoardState {
         new_board[self.zero_pos + 1] = 0;
         new_board[self.zero_pos] = buf;
 
-        return Some(BoardState::new(new_board, self.zero_pos + 1, self.range + 1));
+        return Some(BoardState::new(
+            new_board,
+            self.zero_pos + 1,
+            self.range + 1,
+        ));
     }
-    
+
     fn move_left(&mut self) -> Option<Self> {
         self.left_done = true;
 
@@ -62,7 +67,11 @@ impl BoardState {
         new_board[self.zero_pos - 1] = 0;
         new_board[self.zero_pos] = buf;
 
-        return Some(BoardState::new(new_board, self.zero_pos - 1, self.range + 1));
+        return Some(BoardState::new(
+            new_board,
+            self.zero_pos - 1,
+            self.range + 1,
+        ));
     }
 
     fn move_up(&mut self) -> Option<Self> {
@@ -75,13 +84,16 @@ impl BoardState {
         }
 
         let mut new_board = self.board.clone();
-        let buf = new_board[self.zero_pos - 4];
-        new_board[self.zero_pos - 4] = 0;
+        let buf = new_board[self.zero_pos - BOARD_SIZE];
+        new_board[self.zero_pos - BOARD_SIZE] = 0;
         new_board[self.zero_pos] = buf;
 
-        return Some(BoardState::new(new_board, self.zero_pos - 4, self.range + 1));
+        return Some(BoardState::new(
+            new_board,
+            self.zero_pos - BOARD_SIZE,
+            self.range + 1,
+        ));
     }
-
 
     fn move_down(&mut self) -> Option<Self> {
         self.down_done = true;
@@ -93,11 +105,15 @@ impl BoardState {
         }
 
         let mut new_board = self.board.clone();
-        let buf = new_board[self.zero_pos + 4];
-        new_board[self.zero_pos + 4] = 0;
+        let buf = new_board[self.zero_pos + BOARD_SIZE];
+        new_board[self.zero_pos + BOARD_SIZE] = 0;
         new_board[self.zero_pos] = buf;
 
-        return Some(BoardState::new(new_board, self.zero_pos + 4, self.range + 1));
+        return Some(BoardState::new(
+            new_board,
+            self.zero_pos + BOARD_SIZE,
+            self.range + 1,
+        ));
     }
 
     fn can_move(&self) -> bool {
@@ -109,8 +125,16 @@ impl BoardState {
             self.move_left(),
             self.move_right(),
             self.move_up(),
-            self.move_down()
+            self.move_down(),
         ];
+    }
+
+    fn reset_state(&mut self, other: &BoardState) {
+        self.range = other.range;
+        self.left_done = false;
+        self.right_done = false;
+        self.up_done = false;
+        self.down_done = false;
     }
 
     fn is_single_step_diff(&self, other: &Self) -> bool {
@@ -153,7 +177,6 @@ impl BoardState {
             hash = (i + 1) * board[i] as usize;
         }
 
-
         return BoardState {
             board,
             zero_pos,
@@ -173,12 +196,15 @@ impl BoardState {
     }
 }
 
-
 impl fmt::Display for BoardState {
+    #[rustfmt::skip]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
-            f, 
-            "{:02} {:02} {:02} {:02}\n{:02} {:02} {:02} {:02}\n{:02} {:02} {:02} {:02}\n{:02} {:02} {:02} {:02}\n",
+            f,
+            "{:02} {:02} {:02} {:02}\n\
+            {:02} {:02} {:02} {:02}\n\
+            {:02} {:02} {:02} {:02}\n\
+            {:02} {:02} {:02} {:02}\n",
             self.board[0], self.board[1], self.board[2], self.board[3],
             self.board[4], self.board[5], self.board[6], self.board[7],
             self.board[8], self.board[9], self.board[10], self.board[11],
@@ -188,6 +214,9 @@ impl fmt::Display for BoardState {
 }
 
 fn main() {
+    // this is actually what we want to have at the end
+    // 00 is empty block
+    #[rustfmt::skip]
     let initial_state = BoardState::new_from_board(
         [
             01, 02, 03, 04,
@@ -197,16 +226,17 @@ fn main() {
         ]
     );
 
+    // this is how we starting(whar you see on screen at begining)
+    // 00 is empty block
+    #[rustfmt::skip]
     let end_state = BoardState::new_from_board(
         [
-            01, 06, 02, 04, 
-            05, 00, 03, 07, 
-            14, 13, 10, 08, 
-            09, 15, 12, 11
+            05, 01, 02, 04, 
+            14, 06, 03, 07, 
+            13, 15, 10, 08,
+            09, 00, 12, 11
         ]
     );
-    
-    
 
     let start_time = Instant::now();
     calculate(initial_state, end_state);
@@ -216,9 +246,9 @@ fn main() {
 }
 
 fn calculate(inital_state: BoardState, end_state: BoardState) {
-    let mut states: Vec<Vec<BoardState>> = Vec::with_capacity(16);
+    let mut states: Vec<Vec<BoardState>> = Vec::with_capacity(BOARD_SIZE.pow(2));
 
-    for _ in 0..16 {
+    for _ in 0..BOARD_SIZE.pow(2) {
         states.push(Vec::with_capacity(1_000_000));
     }
 
@@ -226,66 +256,70 @@ fn calculate(inital_state: BoardState, end_state: BoardState) {
 
     let mut solution_found = false;
     let mut new_zero_pos: usize = 0;
+    let mut states_amount: usize = 0;
+    let mut min_range_on_chunk: u32 = 9999;
 
     while !solution_found {
-        let unmoved_state = get_not_activated_state(&mut states, new_zero_pos % 16, &end_state);
+        let unmoved_state =
+            get_not_activated_state(&mut states, new_zero_pos % BOARD_SIZE.pow(2), &end_state);
         new_zero_pos += 1;
 
         for new_state in unmoved_state {
             if let Some(new_state) = new_state {
-                let old_index = states[new_state.zero_pos].iter().position(|x| {
-                    x.range > new_state.range && !x.is_diff_board(&new_state) 
-                });
-
-                if let Some(old_index) = old_index {
-                    states[new_state.zero_pos].remove(old_index);
-                }
-
+                min_range_on_chunk = min(min_range_on_chunk, new_state.range);
                 solution_found = solution_found || !end_state.is_diff_board(&new_state);
-                states[new_state.zero_pos].push(new_state);
-                
-                if get_states_sum(&states) % 1000 == 0 {
-                    let mut min_dist = 99;
-                    for states in &states {
-                        if states.is_empty() {
-                            continue;
-                        }
 
-                        let board = states.iter().min_by_key(|x| x.diff(&end_state)).unwrap();
-                        min_dist = min(
-                            min_dist,
-                            board.diff(&end_state)
-                        );
-                    }
-                    println!("Current states: {}", get_states_sum(&states));
-                    println!("Min diff: {}", min_dist);
+                states_amount =
+                    update_states_list(&mut states[new_state.zero_pos], new_state, states_amount);
+
+                if states_amount % 1000 == 0 {
+                    log_amount_of_states(states_amount, min_range_on_chunk);
+                    min_range_on_chunk = 9999;
                 }
             }
-            
         }
     }
-     
-    print_result(states, end_state);   
+
+    print_result(states, end_state);
 }
 
-fn get_states_sum(arrays: & Vec<Vec<BoardState>>) -> usize {
-    let mut amount = 0;
+fn update_states_list(
+    states: &mut Vec<BoardState>,
+    new_state: BoardState,
+    mut states_amount: usize,
+) -> usize {
+    let old_index = states
+        .iter()
+        .position(|x| x.range > new_state.range && !x.is_diff_board(&new_state));
 
-    for vec in arrays {
-        amount += vec.len(); 
+    if let Some(old_index) = old_index {
+        states[old_index].reset_state(&new_state);
+    } else {
+        states.push(new_state);
+        states_amount += 1;
     }
 
-    return amount;
+    states_amount
 }
 
-fn get_not_activated_state(arrays: &mut Vec<Vec<BoardState>>, index: usize, end_state: &BoardState) -> [Option<BoardState>; 4] {
-    let result_board = arrays[index].iter_mut()
+fn log_amount_of_states(states_amount: usize, min_dist: u32) {
+    println!("Current states: {}", states_amount);
+    println!("Min diff: {}", min_dist);
+}
+
+fn get_not_activated_state(
+    arrays: &mut Vec<Vec<BoardState>>,
+    index: usize,
+    end_state: &BoardState,
+) -> [Option<BoardState>; 4] {
+    let result_board = arrays[index]
+        .iter_mut()
         .filter(|x| x.can_move())
         .min_by_key(|x| x.diff(end_state));
 
     match result_board {
         Some(result) => result.do_move(),
-        None => [None, None, None, None]
+        None => [None, None, None, None],
     }
 }
 
@@ -295,40 +329,42 @@ fn print_result(states: Vec<Vec<BoardState>>, end_state: BoardState) {
     for mut state in states {
         all_states.append(&mut state);
     }
-    
+
     let mut state = all_states.iter().find(|x| !x.is_diff_board(&end_state));
-    
 
     while let Some(unwrapped_state) = state {
         println!("{}", unwrapped_state);
 
-        state = all_states.iter().find(|x| x.range < unwrapped_state.range && unwrapped_state.is_single_step_diff(x));
+        state = all_states
+            .iter()
+            .find(|x| x.range < unwrapped_state.range && unwrapped_state.is_single_step_diff(x));
     }
 }
 
 fn get_xy_pos(zero_pos: usize) -> (usize, usize) {
-    let x = zero_pos % 4;
-    let y = (zero_pos - x) / 4;
+    let x = zero_pos % BOARD_SIZE;
+    let y = (zero_pos - x) / BOARD_SIZE;
 
     return (x, y);
 }
-
 
 #[test]
 fn test_xy_pos() {
     assert_eq!((0, 0), get_xy_pos(0));
     assert_eq!((3, 2), get_xy_pos(11));
- }
+}
 
 #[test]
 fn test_movement() {
-    let mut board_state = BoardState::new(
+    #[rustfmt::skip]
+    let mut board_state = BoardState::new_from_board(
         [
-            1, 2, 3, 4,
-            5, 6, 7, 8,
-            9, 10, 11, 12,
+            01, 02, 03, 04,
+            05, 06, 07, 08,
+            09, 10, 11, 12,
             13, 14, 15, 0
-        ], 15, 0);
+        ]
+    );
 
     let mut new_board = board_state.move_left().unwrap();
     assert_eq!(new_board.board[14], 0);
@@ -341,28 +377,8 @@ fn test_movement() {
     let mut new_board = board_state.move_up().unwrap();
     assert_eq!(new_board.board[11], 0);
     assert_eq!(new_board.board[15], 12);
+
     let new_board = new_board.move_down().unwrap();
     assert_eq!(new_board.board[11], 12);
     assert_eq!(new_board.board[15], 0);
-}
-
-#[test]
-fn generate_test_movement() {
-    let mut board_state = BoardState::new(
-        [
-            1, 2, 3, 4,
-            5, 6, 7, 8,
-            9, 10, 11, 12,
-            13, 14, 15, 0
-        ], 15, 0);
-
-    let mut new_state = board_state.move_up().unwrap();
-    let mut new_state = new_state.move_up().unwrap();
-    let mut new_state = new_state.move_left().unwrap();
-    let mut new_state = new_state.move_left().unwrap();
-    let mut new_state = new_state.move_down().unwrap();
-    let new_state = new_state.move_right().unwrap();
-
-    println!("{}", new_state);
-    
 }
